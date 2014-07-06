ac8608149d4019d9a0fc0332a1ca0444c0e2a270
Sascha Teske <sascha.teske@gmail.com>
2014-07-06 18:17:58 +0200

    First working Version

---
+++ Gruntfile.js
@@ -0,0 +1,90 @@
+'use strict';
+module.exports = function(grunt) {
+  grunt.initConfig({
+    jade: {
+      options: {
+        data: function(dest, src) {
+          return {
+            from: src,
+            to: dest
+          };
+        }
+      },
+      dist: {
+        files: {
+          './result/index.html': './pages/index.jade'
+        }
+      }
+    },
+
+    less: {
+      development: {
+        options: {
+          paths: ["./pages/stylesheets"]
+        },
+        files: {
+          "./result/stylesheets/main.css": "./pages/stylesheets/main.less"
+        }
+      }
+    },
+
+    concat: {
+      main: {
+        files: {
+          './result/javascripts/main.js': './pages/javascripts/**/*.js'
+        }
+      }
+    },
+
+    clean: ['./result/**/*.{html,css,js}'],
+
+    express: {
+        server: {
+          options: {
+            script: './server.js',
+            port: 3080,
+            spawn: true,
+            livereload: true
+          }
+        }
+    },
+
+    watch: {
+      scripts: {
+        files: ['./pages/**/*'],
+        tasks: ['build'],
+      },
+      options: {
+        nospawn: false,
+        atBegin: true,
+        livereload: true
+      }
+    },
+    parallel: {
+      web: {
+        options: {
+        stream: true
+      },
+      tasks: [{
+        grunt: true,
+        args: ['watch']
+      }, {
+        grunt: true,
+        args: ['express']
+      }]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-express-server');
+  grunt.loadNpmTasks('grunt-parallel');
+
+  grunt.registerTask('build', ['clean', 'jade', 'less', 'concat:main']);
+  grunt.registerTask('default', ['parallel:web']);
+};
+
